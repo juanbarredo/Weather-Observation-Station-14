e78db27a9ee5129f231f34b364936dfe4c7828b9
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-01-06 20:25:03 -0500

    01 06 2024
    SOLVED!
    
    Even though this problem and the previous one I worked on were fairly easy to solve.
    
    I still want to solve a bunch more "easy" ones before I move on.
    
    Feels like playing Diablo 2

--- Weather Observation Station 14.sql
+++ Weather Observation Station 14.sql
@@ -51,10 +51,43 @@
 
 --next, I want to get the less than 137.2345
 
-USE Weather_Observation_Station_2;
-SELECT LAT_N
-	FROM STATION
-	WHERE LAT_N < 137.2345;
+--USE Weather_Observation_Station_2;
+--SELECT LAT_N
+--	FROM STATION
+--	WHERE LAT_N < 137.2345;
 
 --ok,
 --I am not going to stop and try to work on uploading since uploading takes a bit longer.
+
+------------------------------------01 06 2024---------------------------------------------
+
+--ok, had some problems but we are up and running.
+--hopefully the computer problems solve themselves for tomorrow
+
+--ok, I have gotten a query that gets me part of the result set.
+--
+--next is I think to use the MAX function on the query
+--I have written up in lines 54 - 57
+
+--USE Weather_Observation_Station_2;
+--SELECT MAX(LAT_N)
+--	FROM STATION
+--	WHERE LAT_N < 137.2345;
+
+--Ok, I believe that I have the result set but now I have to TRUNCATE it to 4 decimal places
+
+--shall I use CAST AS DECIMAL
+
+USE Weather_Observation_Station_2;
+SELECT CAST(MAX(LAT_N) AS DECIMAL(7,4))
+	FROM STATION
+	WHERE LAT_N < 137.2345;
+
+--OK!  SOLVED!	
+
+--Even though this one and the last one were easy to solve.
+--I still feel like I need to explore more of these "easy" problems
+
+--I don't know how many more but I want to just keep going
+--and build solid steps to move through solving these
+
